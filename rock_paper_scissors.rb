b30722675c76f9f01e3479f37cf84dfb4546a565
cornerstonian <vois.cornerstone@gmail.com>
2016-06-07 12:50:18 -0500

    initial commit

---
+++ rock_paper_scissors.rb
@@ -0,0 +1,21 @@
+# Normal Mode
+# The game should ask player 1 for their choice of "rock", "paper", or "scissors". (use gets)
+# The game should then randomly select the computer's selection
+# The game then outputs who won in this format: "Player: ROCK vs Computer: SCISSORS ----- PLAYER WINS"
+
+
+class Game
+  attr_accessor :player_one_choice, :computer
+
+  def initialize
+
+  end
+
+  def play
+
+  end
+
+end
+
+game = Game.new
+game.play
