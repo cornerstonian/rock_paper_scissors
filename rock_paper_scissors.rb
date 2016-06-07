504ca7600c1a7705b827e019af199390ac51e9d7
cornerstonian <vois.cornerstone@gmail.com>
2016-06-07 14:31:03 -0500

    inviting to play, allowing input, outputting incorrect puts statement

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -3,15 +3,33 @@
 # The game should then randomly select the computer's selection
 # The game then outputs who won in this format: "Player: ROCK vs Computer: SCISSORS ----- PLAYER WINS"
 
-
 class Game
   attr_accessor :player_one_choice, :computer
 
   def initialize
-
+    @computer = %w(r p s)
   end
 
   def play
+    puts "Welcome to Rock, Paper, Scsissors \nChoose r for Rock \nChoose p for Paper \nChoose s for Scissors"
+    @player_one_choice = gets.chomp
+    computer_choice = computer.sample
+
+#Why is the first "if" always returning
+
+  if player_one_choice == "r" && computer_choice == "s" or
+     puts "Player: ROCK vs Computer: SCISSORS ----- PLAYER WINS"
+   elsif player_one_choice == "r" && computer_choice == "p"
+     puts "Player: ROCK vs Computer: Paper ----- COMPUTER WINS"
+   elsif player_one_choice == "p" && computer_choice == "r"
+     puts "Player: PAPER vs Computer: rock ----- PLAYER WINS"
+   elsif  player_one_choice == "p" && computer_choice == "s"
+      puts "Player: PAPER vs SCISSORS: ----- COMPUTER WINS"
+    elsif player_one_choice == "s" && computer_choice == "r"
+      puts "Player: SCISSORS vs Rock: ----- COMPUTER WINS"
+    elsif player_one_choice == "s" && computer_choice == "p"
+      puts "Player: SCISSORS vs Paper: ----- PLAYER WINS"
+    end
 
   end
 
@@ -19,3 +37,8 @@ end
 
 game = Game.new
 game.play
+
+
+# Rock beats scissors
+# Paper beats Rock
+# Scissors beats Paper
