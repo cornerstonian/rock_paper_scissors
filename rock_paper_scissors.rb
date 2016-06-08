0dfd328bf564f8bbdcb557d2271657d0024e476c
cornerstonian <vois.cornerstone@gmail.com>
2016-06-08 00:09:53 -0500

    play again, invalid and quit methods added

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -3,6 +3,13 @@
 # The game should then randomly select the computer's selection
 # The game then outputs who won in this format: "Player: ROCK vs Computer: SCISSORS ----- PLAYER WINS"
 
+
+# Epic Mode
+# Instead of asking the player for their choice, have the computer automatically randomly select both the player and computer
+# run the simulation 1000 times
+# Track who which choice wins the most often
+
+
 class Game
     attr_accessor :player_one_choice, :computer
 
@@ -12,7 +19,7 @@ class Game
 
     def play
       puts "Welcome to Rock, Paper, Scsissors \nChoose r for Rock \nChoose p for Paper \nChoose s for Scissors"
-      @player_one_choice = gets.chomp
+      @player_one_choice = gets.chomp.downcase
       computer_choice = computer.sample
 
 
@@ -28,11 +35,34 @@ class Game
             puts "Player: SCISSORS vs Rock: ----- COMPUTER WINS"
           elsif player_one_choice == "s" && computer_choice == "p"
             puts "Player: SCISSORS vs Paper: ----- PLAYER WINS"
-          else
+          elsif player_one_choice == computer_choice
             puts "It's A Tie!"
           end
 
+          play_again?
     end
+
+    def play_again?
+       puts "Would you like to play again? y/n"
+       response = gets.chomp.downcase
+         if response == "y"
+          play
+         elsif response == "n"
+          quit
+         else
+          invalid_command
+         end
+     end
+
+     def invalid_command
+        puts "Sorry, but you command is invalid, Please try again"
+        play
+      end
+
+      def quit
+          puts "Goodbye"
+      end
+
 
 
 end
